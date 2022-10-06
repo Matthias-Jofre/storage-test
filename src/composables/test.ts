0b432762be75469517c5b608cecb21dfcd832501
Matthias <matthias.jofre@gmail.com>
2022-10-06 03:28:08 -0300

    update 5 files

--- src/composables/test.ts
+++ src/composables/test.ts
@@ -12,18 +12,25 @@ const writeSecretFile = async () => {
 const readSecretFile = async () => {
     const contents = await Filesystem.readFile({
         path: 'secrets/text.txt',
-        directory: Directory.Documents,
+        directory: Directory.Data,
         encoding: Encoding.UTF8,
     });
 
     console.log('secrets:', contents);
+    console.log('secrets:', JSON.parse(contents.data));
+    const asd = JSON.parse(contents.data)
+    const asdd = asd.nombre
+    console.log('Ahora puedo mandar mensajes personalizados xd', asd.nombre);
+    return {
+        asdd
+    }
 };
 
 const deleteSecretFile = async () => {
     await Filesystem.deleteFile({
-      path: 'secrets/text.txt',
-      directory: Directory.Documents,
+        path: 'secrets/text.txt',
+        directory: Directory.Documents,
     });
-  };
+};
 
 export { readSecretFile, writeSecretFile, deleteSecretFile }
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,41 +2,60 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>{{test}}</ion-title>
+        <ion-title>Formulario</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+
       <div id="container">
         <!-- <strong>Ready to create an app?</strong>
         <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p> -->
-          
-        <ion-input v-model="test"></ion-input>
 
-        <ion-button @click="secreto">Texto secreto</ion-button>
+        <ion-input v-model="nombre"></ion-input>
+        <ion-input v-model="apellido"></ion-input>
+        <ion-input v-model="correo"></ion-input>
+        <ion-input v-model="comentario"></ion-input>
+
+        <ion-button @click="writeSecretFile">Texto secreto</ion-button>
         <ion-button @click="borrarSecreto">Borrar Secreto</ion-button>
         <ion-button>Guardar ewe</ion-button>
-
+        {{ JSON.stringify(nombreeee) }}
       </div>
-      
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent, ref } from 'vue';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { defineComponent, onBeforeMount } from "vue";
+import { ref, onMounted, watch } from "vue";
 
-import { readSecretFile, writeSecretFile, deleteSecretFile } from '@/composables/test'
+import {
+  readSecretFile,
+  writeSecretFile,
+  deleteSecretFile,
+} from "@/composables/test";
+
+import { Filesystem, Directory, Encoding } from "@capacitor/filesystem";
+
+import { Preferences } from "@capacitor/preferences";
 
 export default defineComponent({
-  name: 'HomePage',
+  name: "HomePage",
   components: {
     IonContent,
     IonHeader,
@@ -44,36 +63,116 @@ export default defineComponent({
     IonTitle,
     IonToolbar,
     IonInput,
-    IonButton
+    IonButton,
   },
   setup() {
+    const usuarios = ref<Formulario[]>([]);
 
-    const test = ref('Blank')
+    const PHOTO_STORAGE = "users";
 
-    const secreto = () => {
-      writeSecretFile()
-    }
+    const cachePhotos = () => {
+      Preferences.set({
+        key: PHOTO_STORAGE,
+        value: JSON.stringify(usuarios.value),
+      });
+    };
+
+    watch(usuarios, cachePhotos);
+
+    const loadSaved = async () => {
+      const photoList = await Preferences.get({ key: PHOTO_STORAGE });
+      const photosInPreferences = photoList.value
+        ? JSON.parse(photoList.value)
+        : [];
+
+      for (const photo of photosInPreferences) {
+        const file = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: Directory.Data,
+        });
+        photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+      }
+      usuarios.value = photosInPreferences;
+    };
+
+    const nombre: any = ref("Nombre");
+    const apellido = ref("Apellido");
+    const correo = ref("Correo");
+    const comentario = ref("Comentario");
+
+    const writeSecretFile = async () => {
+      await Filesystem.writeFile({
+        // path: "secrets/text.txt",
+        path: "secrets/text.txt",
+        data: JSON.stringify({
+          nombre: nombre.value,
+          apellido: apellido.value,
+          correo: correo.value,
+          comentario: comentario.value,
+        }),
+        directory: Directory.Data,
+        encoding: Encoding.UTF8,
+      });
+      // await Filesystem.writeFile({
+      //   // path: "secrets/text.txt",
+      //   path: `nombre/apellido`,
+      //   data: apellido.value,
+      //   directory: Directory.Data,
+      //   encoding: Encoding.UTF8,
+      // });
+      // await Filesystem.writeFile({
+      //   // path: "secrets/text.txt",
+      //   path: `example`,
+      //   data: correo.value,
+      //   directory: Directory.Data,
+      //   encoding: Encoding.UTF8,
+      // });
+      // await Filesystem.writeFile({
+      //   // path: "secrets/text.txt",
+      //   path: `example`,
+      //   data: comentario.value,
+      //   directory: Directory.Data,
+      //   encoding: Encoding.UTF8,
+      // });
+    };
 
     const borrarSecreto = () => {
-      deleteSecretFile()
+      deleteSecretFile();
+    };
+
+    const nombreeee = ref();
+
+    const funcccc = async () => {
+      nombreeee.value = await readSecretFile();
     }
-    
-    readSecretFile()
+
+    funcccc()
 
     return {
-      secreto,
+      writeSecretFile,
       borrarSecreto,
 
-      test
-    }
-  }
+      nombre,
+      apellido,
+      correo,
+      comentario,
+      nombreeee,
+    };
+  },
 });
+
+export interface Formulario {
+  nombre: string;
+  apellido: string;
+  correo: string;
+  comentario: string;
+}
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -89,9 +188,9 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
